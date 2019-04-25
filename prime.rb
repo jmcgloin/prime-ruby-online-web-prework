b60ee986e134865d11c34013044ab540893c94c6
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 22:49:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,12 @@ def prime?(num)
     return false
   end
   testNum = 3
-  mod = 0
   loop do
     mod = num % testNum
-    break if mod == 0 || testNum >= mod
+    if mod == 0
+      return false
+    elsif testNum >= mod
+      return true
+    end
   end
-  return mod == 0 ? true : false
 end
